1624a5385f62ab98a13bcca32424c4a49242736e
QuanwG <minhquanggg792003@gmail.com>
2025-03-21 02:36:59 +0700

    fix category function

--- PharmaDistiPro/Services/Interface/ICategoryService.cs
+++ PharmaDistiPro/Services/Interface/ICategoryService.cs
@@ -8,6 +8,7 @@ namespace PharmaDistiPro.Services.Interface
     {
         Task<Response<CategoryDTO>> CreateCategoryAsync(CategoryInputRequest categoryInputRequest);
         Task<Response<IEnumerable<CategoryDTO>>> FilterCategoriesAsync(string? searchTerm);
+        Task<Response<IEnumerable<CategoryDTO>>> GetAllSubCategoriesAsync();
         Task<Response<IEnumerable<CategoryDTO>>> GetCategoryTreeAsync();
         Task<Response<CategoryDTO>> UpdateCategoryAsync(CategoryInputRequest categoryUpdateRequest);
     }
